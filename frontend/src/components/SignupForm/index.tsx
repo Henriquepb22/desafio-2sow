0dfc2e4fdba006ba3c5efe47e62ffff8cc32d5dd
Henriquepb22 <henriquepbdev@gmail.com>
2020-06-11 22:32:49 -0300

    Removed unused component added toast and impl user signup

--- frontend/src/components/SignupForm/index.tsx
+++ frontend/src/components/SignupForm/index.tsx
@@ -1,61 +1,147 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import FormField from "../FormField";
+import api from "../../services/api";
 
 const SignupForm: React.FC = () => {
+    const [nome, setNome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [email, setEmail] = useState("");
+    const [cep, setCep] = useState("");
+    const [rua, setRua] = useState("");
+    const [numero, setNumero] = useState("");
+    const [bairro, setBairro] = useState("");
+    const [cidade, setCidade] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    async function handleUser(e: FormEvent) {
+        e.preventDefault();
+        setLoading(true);
+
+        await api
+            .post("/usuarios", {
+                nome,
+                cpf,
+                email,
+                endereco: {
+                    cep,
+                    rua,
+                    numero,
+                    bairro,
+                    cidade,
+                },
+            })
+            .then((resp) => {
+                if (resp.status === 201) {
+                    toast.success(`${nome} cadastrado com sucesso!`, {
+                        autoClose: 2000,
+                    });
+                    cleanFields();
+                    setLoading(false);
+                }
+            })
+            .catch((err) => {
+                toast.error(err);
+                setLoading(false);
+            });
+    }
+
+    function cleanFields() {
+        setNome("");
+        setCpf("");
+        setEmail("");
+        setCep("");
+        setRua("");
+        setNumero("");
+        setBairro("");
+        setCidade("");
+    }
+
     return (
-        <form>
-            <span>Dados Pessoais:</span>
-            <FormField
-                name="name"
-                label="Nome:"
-                type="text"
-                placeholder="Insira seu nome"
-            />
-            <FormField
-                name="cpf"
-                label="CPF:"
-                type="text"
-                placeholder="Insira seu CPF"
-            />
-            <FormField
-                name="email"
-                label="E-mail:"
-                type="email"
-                placeholder="Insira seu e-mail"
-            />
-            <span>Endereço:</span>
-            <FormField
-                name="cep"
-                label="CEP:"
-                type="text"
-                placeholder="00000-000"
-            />
-            <FormField
-                name="street"
-                label="Rua:"
-                type="text"
-                placeholder="Ex: Avenida Paulista"
-            />
-            <FormField
-                name="number"
-                label="Número:"
-                type="text"
-                placeholder="Ex: 123A"
-            />
-            <FormField
-                name="neighborhood"
-                label="Bairro:"
-                type="text"
-                placeholder="Ex: Campinas"
-            />
-            <FormField
-                name="city"
-                label="Cidade:"
-                type="text"
-                placeholder="Ex: São Paulo"
-            />
-            <button type="submit">Cadastrar</button>
+        <form onSubmit={(e) => handleUser(e)}>
+            <ToastContainer />
+            <fieldset>
+                <legend>Dados Pessoais:</legend>
+                <label htmlFor="nome">Nome:</label>
+                <input
+                    type="text"
+                    id="nome"
+                    placeholder="Insira seu nome"
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+                    required
+                />
+                <label htmlFor="cpf">CPF:</label>
+                <input
+                    type="text"
+                    id="cpf"
+                    placeholder="Insira seu CPF"
+                    value={cpf}
+                    onChange={(e) => setCpf(e.target.value)}
+                    required
+                />
+                <label htmlFor="email">E-mail:</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Insira seu e-mail"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+            </fieldset>
+            <fieldset>
+                <legend>Endereço:</legend>
+                <label htmlFor="cep">CEP:</label>
+                <input
+                    type="text"
+                    id="cep"
+                    placeholder="00000-000"
+                    value={cep}
+                    onChange={(e) => setCep(e.target.value)}
+                    required
+                />
+                <label htmlFor="rua">Rua:</label>
+                <input
+                    type="text"
+                    id="rua"
+                    placeholder="Ex: Avenida Paulista"
+                    value={rua}
+                    onChange={(e) => setRua(e.target.value)}
+                    required
+                />
+                <label htmlFor="numero">Número:</label>
+                <input
+                    type="text"
+                    id="numero"
+                    placeholder="Ex: 123A"
+                    value={numero}
+                    onChange={(e) => setNumero(e.target.value)}
+                    required
+                />
+                <label htmlFor="bairro">Bairro:</label>
+                <input
+                    type="text"
+                    id="bairro"
+                    placeholder="Ex: Centro"
+                    value={bairro}
+                    onChange={(e) => setBairro(e.target.value)}
+                    required
+                />
+                <label htmlFor="cidade">Cidade:</label>
+                <input
+                    type="text"
+                    id="cidade"
+                    placeholder="Ex: São Paulo"
+                    value={cidade}
+                    onChange={(e) => setCidade(e.target.value)}
+                    required
+                />
+            </fieldset>
+            <button type="submit" disabled={loading}>
+                Cadastrar
+            </button>
         </form>
     );
 };
