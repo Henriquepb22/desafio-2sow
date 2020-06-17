e02c07a16a79603f58d566b5d855cf729a5d0909
Henriquepb22 <henriquepbdev@gmail.com>
2020-06-17 15:06:24 -0300

    Added responsiveness to header

--- frontend/src/components/Header/styled.ts
+++ frontend/src/components/Header/styled.ts
@@ -10,14 +10,32 @@ export const HeaderContainer = styled.header`
 `;
 
 export const HeaderTitle = styled.h1`
-    margin-left: 2.2rem;
+    margin-left: 0.8rem;
     color: white;
-    font-size: 2rem;
+    font-size: 1.8rem;
     font-weight: bold;
+
+    @media (min-width: 768px) {
+        margin-left: 1.2rem;
+        font-size: 2rem;
+    }
+
+    @media (min-width: 992px) {
+        margin-left: 1.8rem;
+        font-size: 2.2rem;
+    }
 `;
 
 export const HeaderNav = styled.nav`
-    margin-right: 2rem;
+    margin-right: 0.8rem;
+
+    @media (min-width: 768px) {
+        margin-right: 1.2rem;
+    }
+
+    @media (min-width: 992px) {
+        margin-right: 1.8rem;
+    }
 `;
 
 export const HeaderList = styled.ul`
@@ -25,12 +43,25 @@ export const HeaderList = styled.ul`
 `;
 
 export const HeaderLinks = styled.li`
-    margin: 0 0.8rem;
+    margin: 0 0.4rem;
+    white-space: nowrap;
+
+    @media (min-width: 768px) {
+        margin: 0 0.6rem;
+    }
+
+    @media (min-width: 992px) {
+        margin: 0 0.8rem;
+    }
 
     a {
         padding: 0.6rem;
         font-weight: bold;
         text-decoration: none;
         color: white;
+
+        &:hover {
+            border-bottom: 2px solid #ddd;
+        }
     }
 `;
